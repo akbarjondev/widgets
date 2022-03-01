53ab70d4e6c3cbde9b6d5606584188af83b037ad
Akbarjon <tojiyev.akbarjon@gmail.com>
2022-03-01 20:33:37 +0500

    Initial commit

---
+++ src/components/Accordion.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Accordion = ({items}) => {
+
+  return (
+    <div>{items.length}</div>
+  );
+}
+
+export default Accordion;
---
+++ src/components/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const items = [
+  {
+    title: 'React nima?',
+    description: 'React javascript kutubxona'
+  },
+  {
+    title: 'Nima uchun React ishlatish kk?',
+    description: 'React ko`pchilik dasturchilar tomonidan sevib foydalaniladi'
+  },
+  {
+    title: 'JSX nima?',
+    description: 'JavaScript uchun sintaks'
+  }
+]
+
+const App = () => {
+
+  return (
+    <div>Salom</div>
+  );
+}
+
+export default App;
