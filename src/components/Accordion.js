a96d48f2eb4d2b886e00cc597d1b084e62d21c1b
Akbarjon <tojiyev.akbarjon@gmail.com>
2022-03-09 09:53:53 +0500

    Add Accordion

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,9 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Accordion = ({items}) => {
 
+  const [activeIndex, setActiveIndex] = useState(null);
+
+  const clickHandler = index => {
+    setActiveIndex(index);
+  }
+
+  const renderedItems = items.map((item, index) => {
+    const active = index === activeIndex ? 'active' : '';
+
+    return <React.Fragment key={item.title}>
+      <div 
+        className={`title ${active}`}
+        onClick={() => clickHandler(index)}
+      >
+        <i className='dropdown icon'></i>
+        {item.title}
+      </div>
+      <div className={`content ${active}`}>
+        <p>{item.description}</p>
+      </div>
+    </React.Fragment>
+  })
+
   return (
-    <div>{items.length}</div>
+    <div className='ui styled accordion'>
+      {renderedItems}
+    </div>
   );
 }
 
